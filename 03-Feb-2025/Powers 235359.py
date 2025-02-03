325d243d2660473bb8dc2167097f7093610502ab
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-03 10:08:52 +0300

    Created 'Powers' by A2SV Hub

---
+++ 03-Feb-2025/Powers 235359.py
@@ -0,0 +1,8 @@
+# Problem: Powers - https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+a = int(input())  
+b = int(input())  
+m = int(input())  
+
+print(a ** b)       
+print((a ** b) % m)
