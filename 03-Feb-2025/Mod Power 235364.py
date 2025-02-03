42632ba44249a0c66de9e1016f5c6939fb670117
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-03 10:13:08 +0300

    Created 'Mod Power' by A2SV Hub

---
+++ 03-Feb-2025/Mod Power 235364.py
@@ -0,0 +1,8 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+a = int(input())  
+b = int(input())  
+m = int(input()) 
+
+print(a ** b)        
+print((a ** b) % m)  
