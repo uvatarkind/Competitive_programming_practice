a3b3cdcacaa028eefaf85c6a99dffd61e15a5e81
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-11 15:34:42 +0300

    Created 'Counting Sort ' by A2SV Hub

---
+++ 11-Feb-2025/Counting Sort  256861.py
@@ -0,0 +1,35 @@
+# Problem: Counting Sort  - https://www.hackerrank.com/challenges/countingsort1/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    freq=[0]*len(arr)
+    for num in arr:
+        freq[num]+=1
+    return freq
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
