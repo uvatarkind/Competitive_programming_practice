22cc9fe9a19226bb9359e4e644d70635cd518a34
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-11 16:25:47 +0300

    Created 'Sort an Array' by A2SV Hub

---
+++ 11-Feb-2025/Sort an Array 257233.py
@@ -0,0 +1,13 @@
+# Problem: Sort an Array - https://leetcode.com/problems/sort-an-array/description/
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        arr=[0]*(10**5+1)
+        for i in nums:
+            arr[i+(50000)]+=1
+        new=[]
+        for i in range(len(arr)):
+            if arr[i]:
+                for j in range(arr[i]):
+                    new.append(i-50000)
+        return new
