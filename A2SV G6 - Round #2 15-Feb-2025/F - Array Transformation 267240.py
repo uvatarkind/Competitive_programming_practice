8228b9998327a66ebeab17c5c017af9328930947
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-15 21:29:11 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 15-Feb-2025/F - Array Transformation 267240.py
@@ -0,0 +1,16 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+t= int(input())
+for _ in range(t):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    has=Counter(arr)
+    mx=0
+    maxsum=0
+    new=Counter(has.values())
+    sorted_data_desc = list(sorted(new.keys(), reverse=True))
+    for key in sorted_data_desc:
+        maxsum+=new[key]
+        mx=max(mx,(key*maxsum))
+    print(n-mx)
