6ea57bdafd9ba6da6ca663c1d747eb4da51af94b
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-15 21:28:49 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 15-Feb-2025/A - Make the Product Zero 267237.py
@@ -0,0 +1,19 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+n=int(input())
+mn=float("inf")
+mx=float("-inf")
+arr= list(map(int,input().split()))
+for x in arr:
+    if x<0:
+        mx=max(x,mx)
+    elif x>0:
+        mn=min(mn,x)
+    if x==0:
+        print(0)
+        exit()
+                       
+if mx*(-1)>=mn:
+    print(mn)
+elif mx*(-1)<mn:
+    print(mx*(-1))
