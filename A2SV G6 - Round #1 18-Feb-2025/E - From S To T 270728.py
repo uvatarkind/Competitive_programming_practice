028a864c00a1090a6cbe3fd3f4fea0c540fedc7a
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-18 12:25:34 +0300

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 18-Feb-2025/E - From S To T 270728.py
@@ -0,0 +1,26 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+from collections import Counter
+
+t = int(input().strip())
+
+for _ in range(t):
+    s = input().strip()
+    n = input().strip()
+    p = input().strip()
+    
+    it = iter(n)
+    if not all(char in it for char in s):
+        print("NO")
+        continue 
+    count_s = Counter(s)
+    count_n = Counter(n)
+    count_p = Counter(p)
+    for char in count_n:
+        required = count_n[char]
+        available = count_s.get(char, 0) + count_p.get(char, 0)
+        if required > available:
+            print("NO")
+            break
+    else:
+        print("YES")
