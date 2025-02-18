96cc86278a34397228ee86efe17371b297f15d83
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-18 11:01:58 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 18-Feb-2025/C - The Splitting Game 270480.py
@@ -0,0 +1,19 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    s=input()
+    char=Counter(s)
+    summ=0
+    lc=len(char)
+    new=set()
+    
+    for x in s:
+        char[x]-=1
+        new.add(x)
+        if char[x]==0:
+            del char[x]
+        lc=max(lc,len(char)+len(new))
+    print(lc) 
