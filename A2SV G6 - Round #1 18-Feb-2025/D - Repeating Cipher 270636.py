5ac25f1d1ec2b81cf7f05ab9751f3aab5972a15b
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-18 11:41:21 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 18-Feb-2025/D - Repeating Cipher 270636.py
@@ -0,0 +1,12 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n=int(input())
+s=input()
+ans=""
+c=1
+i=0
+while i<len(s):
+    ans+=(s[i])
+    c+=1
+    i+=c
+print(ans)
