84d4d8d0312387a04218a84fd31ccd4d11110244
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-19 18:35:49 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 19-Feb-2025/B - Anansi and Trip-Photographs 274642.py
@@ -0,0 +1,18 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+t= int(input())
+for _ in range(t):
+    n,x=map(int,input().split())
+    arr=list(map(int,input().split()))
+    arr.sort()
+    l=0
+    r=n
+    valid=True
+    while l<r:
+        if arr[r+l]-arr[l]<x:
+            valid=False
+        l+=1
+    if valid:
+        print("YES")
+    else:
+        print("NO")
