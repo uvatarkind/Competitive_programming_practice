85c9a72bdd1538396574c0b1b2512bdc49026484
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-21 10:43:15 +0300

    Created 'Books' by A2SV Hub

---
+++ 21-Feb-2025/Books 278119.py
@@ -0,0 +1,21 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+arr = list(map(int, input().split()))
+
+left = 0
+right = 0
+summ = 0
+mx = 0
+
+while right < n:
+    
+    if summ + arr[right] <= t:
+        summ += arr[right]
+        right += 1
+        mx = max(mx, right - left) 
+    else:
+        summ -= arr[left]
+        left += 1  
+
+print(mx)
