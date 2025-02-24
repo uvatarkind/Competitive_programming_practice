c5695902496e15984fcbe343615b6cbc507ca894
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-02-24 20:41:47 +0300

    Created 'Sort The Students By Their Kth Score' by A2SV Hub

---
+++ 24-Feb-2025/Sort The Students By Their Kth Score 281846.py
@@ -0,0 +1,17 @@
+# Problem: Sort The Students By Their Kth Score - https://leetcode.com/problems/sort-the-students-by-their-kth-score/
+
+class Solution:
+    def sortTheStudents(self, a: List[List[int]], k: int) -> List[List[int]]:
+        v=[]
+        
+        for i in range(len(a)):
+            v+=[[a[i][k],a[i]]]
+        print(v)
+        def cmp(a):
+            return a[0]
+        v=sorted(v,key=cmp)[::-1]
+        print(v)
+        ans=[]
+        for i in range(len(v)):
+            a[i]=v[i][1]
+        return a
