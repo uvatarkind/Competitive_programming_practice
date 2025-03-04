622fe7d4202b56aebd9d0473dd8f47d65d24964c
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-03-04 13:02:46 +0300

    Created 'D - Bernabas and the Harmonious Melody' by A2SV Hub

---
+++ A2SV G6 - Round #3 04-Mar-2025/D - Bernabas and the Harmonious Melody 290818.py
@@ -0,0 +1,35 @@
+# Problem: D - Bernabas and the Harmonious Melody - https://codeforces.com/gym/588468/problem/D
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  s = input()
+  data = set(s)
+  char = ""
+  mncnt = float("inf")
+  
+  for char in data:
+    l, r = 0, n - 1
+    cnt = 0
+    x = 1
+    
+    while l < r:
+      if s[l] == s[r]:
+        l, r = l + 1, r - 1
+        continue
+      
+      if s[l] != char and s[r] != char:
+        x = 0
+        break
+    
+      else:
+        cnt += 1
+        if s[l] == char:
+          l += 1
+        else:
+          r -= 1
+          
+    if x:
+      mncnt = min(mncnt, cnt)
+      
+  print(mncnt if mncnt != float("inf") else -1)
