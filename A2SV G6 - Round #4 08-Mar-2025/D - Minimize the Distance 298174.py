5378038b175cbfb9b473f0b8688625f7e72809a0
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-03-08 22:15:27 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 08-Mar-2025/D - Minimize the Distance 298174.py
@@ -0,0 +1,11 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n=int(input())
+
+arr=list(map(int,input().split()))
+arr.sort()
+if len(arr)%2==0:
+    print(arr[n//2-1])
+    
+else: 
+    print(arr[n//2])
