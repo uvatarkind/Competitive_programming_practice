72ec35239a5633e160bf6cfe9d5a65ca13e0bd7f
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-03-09 22:44:40 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 09-Mar-2025/A - Language Identifier 298977.py
@@ -0,0 +1,12 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+t= int(input())
+for _ in range(t):
+    s=input()
+    n=len(s)
+    if s[n-1]=='o':
+        print('FILIPINO')
+    elif s[n-1]=='u':
+        print('JAPANESE')
+    else:
+        print('KOREAN')
