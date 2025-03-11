6c41d2f2841539882a105dd38851a865eac93e93
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-03-11 18:23:49 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/D - Meklit's Chat Screen 302762.py
@@ -0,0 +1,20 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+new_arr = deque()  
+seen = set()  
+
+for num in arr:
+    if num not in seen:
+        if len(new_arr) >= k:
+            removed = new_arr.popleft() 
+            seen.remove(removed) 
+        new_arr.append(num)
+        seen.add(num)
+
+print(len(new_arr))
+print(*reversed(new_arr))  
