ca4ad9a6b71411b144b1b9a01cf20501b7970878
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-03-16 23:44:30 +0300

    Created 'Decode String' by A2SV Hub

---
+++ 16-Mar-2025/Decode String 308665.py
@@ -0,0 +1,20 @@
+# Problem: Decode String - https://leetcode.com/problems/decode-string/
+
+class Solution(object):
+    def decodeString(self, s):
+        stack = []; curNum = 0; curString = ''
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+            elif c.isdigit():
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+        return curString
