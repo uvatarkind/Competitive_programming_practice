88a87f98e6ddf18f9f27b44a3f6a123d8fc63ae8
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-03-18 17:09:03 +0300

    Created 'Next Greater Element' by A2SV Hub

---
+++ 18-Mar-2025/Next Greater Element 311028.py
@@ -0,0 +1,28 @@
+# Problem: Next Greater Element - https://leetcode.com/problems/next-greater-element-i/
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        hashmap = {}
+        output = []
+
+        for i in reversed(nums2):
+            while stack:
+                
+                if stack[-1] > i:
+                    hashmap[i] = stack[-1]
+                    stack.append(i)
+                    break
+                else: 
+                    stack.pop()
+            
+
+            if not stack:
+                hashmap[i] = -1
+                stack.append(i)
+        
+        for j in nums1:
+            output.append(hashmap[j])
+        return output
+
+        
