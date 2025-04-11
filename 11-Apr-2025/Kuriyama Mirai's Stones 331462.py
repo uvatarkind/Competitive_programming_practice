db6884943cc2e4e4e86143b4f90f882219e00031
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-04-11 04:52:59 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 11-Apr-2025/Kuriyama Mirai's Stones 331462.py
@@ -0,0 +1,22 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n = int(input())
+given = list(map(int, input().split()))
+
+prefix_given = [0] * (n + 1)
+for i in range(n):
+    prefix_given[i + 1] = prefix_given[i] + given[i]
+
+cost = sorted(given)
+prefix_cost = [0] * (n + 1)
+for i in range(n):
+    prefix_cost[i + 1] = prefix_cost[i] + cost[i]
+
+question = int(input())
+for _ in range(question):
+    q = list(map(int, input().split()))
+    l, r = q[1], q[2]
+    if q[0] == 1:
+        print(prefix_given[r] - prefix_given[l - 1])
+    else:
+        print(prefix_cost[r] - prefix_cost[l - 1])
