794e42b0fc51dbc879921c4f608b5b8830d3d280
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-04-11 04:04:17 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 11-Apr-2025/Red and Blue 331459.py
@@ -0,0 +1,23 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t=int(input())
+for _ in range(t):
+    n= int(input())
+    r=list(map(int,input().split()))
+    m= int(input())
+    b=list(map(int,input().split()))
+    
+    def max_pref(a):
+        mx=0
+        curr=0
+        for num in a:
+            curr+=num
+            mx=max(mx,curr)
+       
+        return mx
+        
+    
+    red= max_pref(r)
+    blue=max_pref(b)
+    
+    print(max(red,blue,red+blue))
