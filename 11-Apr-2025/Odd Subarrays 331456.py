dff833323ba7fcaa43d0e3b71a1e1ebc4b0e1600
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-04-11 03:24:41 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 11-Apr-2025/Odd Subarrays 331456.py
@@ -0,0 +1,17 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t=int(input())
+for _ in range(t):
+    n= int(input())
+    p=list(map(int,input().split()))
+    i=1
+    count=0
+    while i<n:
+        if p[i-1]>p[i]:
+          count+=1
+          i+=2
+        else:
+            i+=1
+    print(count)
+        
+    
