4161670f67959097a7a17cec481b0dd9529178da
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-05-16 19:13:49 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 16-May-2025/Cirno's Perfect Bitmasks Classroom 357079.py
@@ -0,0 +1,15 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t=int(input())
+for _ in range(t):
+    x=int(input())
+    if x==1:
+        print(3)
+    else:
+        if x>0 and x&(x-1)==0:
+            print(x+1)
+        else:
+            for i in range(32):
+                if x & (1<<i):
+                    print(1<<i)
+                    break
