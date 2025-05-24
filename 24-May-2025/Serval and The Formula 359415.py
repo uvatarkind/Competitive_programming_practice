41fe7de9dd7ac5319092cb180824b2406c1262f7
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-05-24 15:10:45 +0300

    Created 'Serval and The Formula' by A2SV Hub

---
+++ 24-May-2025/Serval and The Formula 359415.py
@@ -0,0 +1,14 @@
+# Problem: Serval and The Formula - https://codeforces.com/problemset/problem/2085/C
+
+t= int(input())
+for _ in range(t):
+    x,y= map(int,input().split())
+    if x==y:
+        print(-1)
+    else:
+        if x<y:
+            x,y=y,x
+        i=1
+        while i<x:
+            i=i*2
+        print(i-x)
