fe815a412e58cb73b58167f9a719f31745924135
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-05-31 03:23:20 +0300

    Created 'Spanning Tree' by A2SV Hub

---
+++ 31-May-2025/Spanning Tree 361527.py
@@ -0,0 +1,58 @@
+# Problem: Spanning Tree - https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/E
+
+import sys
+input = sys.stdin.read
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0]*n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        xr = self.find(x)
+        yr = self.find(y)
+        if xr == yr:
+            return False
+        if self.rank[xr] < self.rank[yr]:
+            self.parent[xr] = yr
+        else:
+            self.parent[yr] = xr
+            if self.rank[xr] == self.rank[yr]:
+                self.rank[xr] += 1
+        return True
+
+def minimum_spanning_tree_weight(n, edges):
+    edges.sort(key=lambda x: x[2])  # sort by weight
+    uf = UnionFind(n)
+    total_weight = 0
+    count = 0
+
+    for u, v, w in edges:
+        if uf.union(u - 1, v - 1):  # -1 to make zero-based
+            total_weight += w
+            count += 1
+            if count == n - 1:
+                break
+    return total_weight
+
+# Main input parsing
+def main():
+    data = input().split()
+    n = int(data[0])
+    m = int(data[1])
+    edges = []
+    index = 2
+    for _ in range(m):
+        u = int(data[index])
+        v = int(data[index + 1])
+        w = int(data[index + 2])
+        edges.append((u, v, w))
+        index += 3
+    print(minimum_spanning_tree_weight(n, edges))
+
+main()
