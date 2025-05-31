8891197d9043b200ef004c394e8a096c79541e2e
Yeabsira Mekonnen <yeabsira.mekonnen.w@gmail.com>
2025-05-31 03:18:46 +0300

    Created 'Satisfiability of Equality Equations' by A2SV Hub

---
+++ 31-May-2025/Satisfiability of Equality Equations 361526.py
@@ -0,0 +1,24 @@
+# Problem: Satisfiability of Equality Equations - https://leetcode.com/problems/satisfiability-of-equality-equations/
+
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        parent = {}
+        def find(x):
+            if x not in parent:
+                return x
+            else:
+                return find(parent[x])
+        for i in equations:
+            if i[1]=="=":
+                a = i[0]
+                b = i[-1]
+                x = find(a)
+                y = find(b)
+                if x!=y:
+                    parent[y] = x
+        for i in equations:
+            if i[1]=="!" and find(i[0])==find(i[-1]):
+                return False
+        return True
+
+        
