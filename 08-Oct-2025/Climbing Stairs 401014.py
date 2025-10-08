dbf4f46873853f7fe95df35f712a133dc109b06f
Yeabsira Mekonnen <yubi14465@gmail.com>
2025-10-08 17:12:19 +0300

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 08-Oct-2025/Climbing Stairs 401014.py
@@ -0,0 +1,8 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return b 
